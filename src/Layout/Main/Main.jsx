4245b37de5baf33f500b4f8876d9c745d37cb94b
RAHAT HASAN DIPO <rahathasandipto@gmail.com>
2022-10-25 23:39:11 +0600

    first commit

---
+++ src/Layout/Main/Main.jsx
@@ -0,0 +1,14 @@
+import React from 'react';
+import Header from '../../Pages/Header/Header';
+
+const Main = () => {
+    return (
+        <div>
+            <Header />
+            <h1>Main</h1>
+            <h1>Main</h1>
+        </div>
+    );
+};
+
+export default Main;
